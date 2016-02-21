c3708489ae94dcd8ebab00194450c73ad053dbca
Charles Lehner <cel@celehner.com>
2016-02-21 12:29:57 -0500

    Document methods. Add commit/tag parsing

--- README.md
+++ README.md
@@ -7,32 +7,122 @@ utility methods
 
 Below, `source(obj)` means returns a [readable stream][pull-stream] for objects of type `obj`
 
-#### `repo.readCommit(hash): source({name, value || read})`
-#### `repo.readTag(hash): source({name, value || read})`
+#### `Repo(repo): repo`
 
-Read a commit or tag. Returns a readable stream of objects for fields in the
-commit.  The commit/tag message is treated as a field of type `"body"` except
-it comes with a `read` function instead of a `value` string.
+Mixin `pull-git-repo` methods into `repo`
 
-- `hash`: SHA1 hash ID of the commit to read
+#### `repo.readCommit(rev): source({name, value})`
+
+Read a commit. Returns a readable stream of objects for fields in the
+commit.  The commit message is treated as a field of type `"title"` for the
+first line, and a field of type `"body"` for the rest.
+
+- `rev`: SHA1 hash of the commit to read, or a ref pointing to it
 - `name`: name of a field, one of
     `["tree", "parent", "author", "committer", "body"]`
 - `value`: string value of the field
-- `read`: source for the raw data of the commit/tag body, if `name == "body"`
+
+#### `repo.readTag(rev): source({name, value})`
+
+Read a tag. Returns a readable stream of objects for fields in the
+tag.  The tag message is treated as a field of type `"title"` for the
+first line, and a field of type `"body"` for the rest.
+
+- `rev`: SHA1 hash of the tag to read, or a ref pointing to it
+- `name`: name of a field, one of
+    `["object", "type", "tag", "tagger", "title", "body"]`
+- `value`: string value of the field
 
 #### `repo.readTree(rev): source({id, mode, name})`
 
-#### `repo.readDir(branch, path): source({id, mode, name})`
+Get a tree and stream its entries
+
+#### `repo.readDir(rev, path): source({id, mode, name})`
+
+Stream entries from a tree down a given path
+
+#### `repo.readLog(head): source(hash)`
+
+Stream commit IDs of the repo, following the commit history backwards
+
+- `head`: hash or rev of the commit from which to start reading history
+
+#### `repo.resolveRef(name, cb(err, hash))`
+
+Get the hash that a ref points to. Errors if the ref is not found.
+
+#### `repo.getRef(name, cb(err, object))`
+
+Get a git object
+
+- `name`: name of a ref pointing to the object, or SHA1 of the object
+
+#### `repo.getCommit(rev, cb(err, object))`
+
+Get a commit object. If the object refered to by `rev` is a tag, get the commit
+that it points to.
+
+#### `repo.getTag(rev, cb(err, object))`
+
+Get a tag object.
 
 #### `repo.getTree(rev, cb(err, object))`
 
-#### `repo.getFile(branch, path, cb(err, {length, mode, read})`
+Get a tree object. If `rev` refers to a commit, get the tree that the
+commit refers to.
 
-#### `repo.readLog(head, limit): source(hash)`
+#### `repo.getCommitParsed(rev, cb(err, commit))`
 
-#### `repo.resolveRef(name, cb)`
+Get a commit buffered and parsed into a JSON object
 
-#### `repo.getRef(name, cb)`
+- `commit.id`: ID of the commit
+- `commit.tree`: ID of the tree of the commit
+- `commit.parents`: IDs of parent commits. There will be more than one if it is
+  a merge commit.
+- `commit.title`: first line of the commit message
+- `commit.body`: text from the commit message following the first line and an
+  optional blank line
+- `commit.author`: `user` object for info about the commit author
+- `commit.committer`: `user` object for info about the committer
+- `commit.separateAuthor`: convenience value indicating `commit.author` and
+  `commit.committer` are different
+
+Example:
+
+```js
+{
+  "parents": [
+    "f7c37c43a136064e07328ee7501fad8ed7bcc4d6"
+  ],
+  "author": {
+    "str": "root <root@localhost> 1455078653 -0500",
+    "name": "root",
+    "email": "root@localhost",
+    "date": new Date(1455078653)
+  },
+  "committer": {
+    "str": "root <root@localhost> 1455078653 -0500",
+    "name": "root",
+    "email": "root@localhost",
+    "date": new Date(1455078653)
+  },
+  "body": "",
+  "id": "9a385c1d6b48b7f472ac507a3ec08263358e9804",
+  "tree": "68aba62e560c0ebc3396e8ae9335232cd93a3f60",
+  "title": "Initial commit",
+  "separateAuthor": false
+}
+```
+
+#### `repo.getFile(rev, path, cb(err, {length, mode, read)`
+
+Get a file from tree at the given path.
+
+`length`: size of the file in bytes
+`mode`: mode of the file, e.g. `"100644"`
+`read`: readable stream of the file's contents
+
+#### `repo.isCommitHash(str): bool`
 
 [pull-stream]: https://github.com/dominictarr/pull-stream/
 [abstract-pull-git-repo]: https://github.com/clehner/abstract-pull-git-repo
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var buffered = require('pull-buffered')
 var pull = require('pull-stream')
+var util = require('util')
 
 var R = {}
 
@@ -9,9 +10,18 @@ module.exports = function (repo) {
   return repo
 }
 
+module.exports.NotFoundError = NotFoundError
+
+function NotFoundError(msg) {
+  var err = Error.call(this, msg)
+  err.name = NotFoundError.name
+  return err
+}
+util.inherits(NotFoundError, Error)
+
 function split2(str, delim) {
   var i = str.indexOf(delim || ' ')
-  return i === -1 ? [str] : [str.substr(0, i), str.substr(i + 1)]
+  return i === -1 ? [str, ''] : [str.substr(0, i), str.substr(i + 1)]
 }
 
 function readNext(fn) {
@@ -26,7 +36,79 @@ function readNext(fn) {
   }
 }
 
-function isCommitHash(str) {
+function buffer(read, cb) {
+  pull(
+    read,
+    pull.collect(function (err, bufs) {
+      cb(err, bufs && Buffer.concat(bufs))
+    })
+  )
+}
+
+function parseCommitOrTag(object, id) {
+  var body = '', state = 'begin'
+  var b = buffered(object.read)
+  return function read(end, cb) {
+    switch (state) {
+      case 'begin':
+        state = 'fields'
+        return cb(null, {name: 'id', value: id})
+      case 'body':
+        state = 'end'
+        return buffer(b.passthrough, function (err, data) {
+          if (err) return cb(err)
+          body += data.toString('utf8')
+          cb(null, {name: 'body', value: body})
+        })
+      case 'fields':
+        return b.lines(end, function (err, line) {
+          if (err) return cb(err)
+          if (line === '') {
+            state = 'title'
+            read(null, cb)
+          } else {
+            var s = split2(line)
+            var name = s[0]
+            var value = s[1]
+            switch (name) {
+              case 'author':
+              case 'committer':
+              case 'tagger':
+                var m = value.match(/^(.*) <(.*)> (.*) (.*)$/) || [, value]
+                value = {
+                  str: value,
+                  name: m[1],
+                  email: m[2],
+                  date: new Date(m[3] * 1000)
+                }
+            }
+            cb(null, {name: name, value: value})
+          }
+        })
+      case 'title':
+        state = 'newline'
+        return b.lines(end, function (err, line) {
+          if (err) return cb(err)
+          cb(null, {name: 'title', value: line})
+        })
+      case 'newline':
+        state = 'body'
+        return b.lines(end, function (err, line) {
+          if (err) return cb(err)
+          if (line === '') {
+            read(end, cb)
+          } else {
+            body = line + '\n'
+            read(end, cb)
+          }
+        })
+      default:
+        return cb(state)
+    }
+  }
+}
+
+R.isCommitHash = function (str) {
   return /[0-9a-f]{20}/.test(str)
 }
 
@@ -34,7 +116,7 @@ R.resolveRef = function (name, cb) {
   if (!name)
     return cb(new Error('Invalid name'))
 
-  if (isCommitHash(name))
+  if (this.isCommitHash(name))
     return cb(null, name)
 
   if (name.indexOf('/') === -1)
@@ -43,7 +125,7 @@ R.resolveRef = function (name, cb) {
   var readRef = this.refs()
   readRef(null, function next(end, ref) {
     if (end)
-      cb(new Error('Ref ' + ref + ' not found'))
+      cb(new NotFoundError('Ref \'' + name + '\' not found'))
     else if (ref.name !== name)
       readRef(null, next)
     else
@@ -57,35 +139,27 @@ R.getRef = function (name, cb) {
   this.resolveRef(name, function (err, hash) {
     if (err) return cb(err)
     this.getObject(hash, cb)
-      // if (!err && !hash)
-        // return cb(new Error('Ref ' + name + ' not found'))
   }.bind(this))
 }
 
-R.readCommit = R.readTag = function (hash) {
-  var ended, b, next
-
-  return function read(end, cb) {
-    if (ended) return cb(ended)
-    if (b) return next(end, cb)
-    this.getObject(hash, function (err, object) {
+R.readCommit = function (rev) {
+  var self = this
+  return readNext(function (cb) {
+    self.getCommit(rev, function (err, object, hash) {
       if (err) return cb(err)
-      b = buffered(object.read)
-      next(null, cb)
+      cb(null, parseCommitOrTag(object, hash))
     })
-  }.bind(this)
+  })
+}
 
-  function next(end, cb) {
-    b.lines(end, function (err, line) {
-      if (line) {
-        var s = split2(line)
-        cb(null, {name: s[0], value: s[1]})
-      } else {
-        ended = true
-        cb(null, {name: 'body', read: b.passthrough})
-      }
+R.readTag = function (rev) {
+  var self = this
+  return readNext(function (cb) {
+    self.getCommit(rev, function (err, object, hash) {
+      if (err) return cb(err)
+      cb(null, parseCommitOrTag(object, hash))
     })
-  }
+  })
 }
 
 function readCommitOrTagProperty(object, property, cb) {
@@ -103,6 +177,27 @@ function readCommitOrTagProperty(object, property, cb) {
       readLine(null, next)
     }
   })
+}
+
+R.getCommitParsed = function (ref, cb) {
+  var commit = {
+    parents: [],
+    author: {},
+    committer: {},
+    body: ''
+  }
+  pull(
+    this.readCommit(ref),
+    pull.drain(function (field) {
+      if (field.name == 'parent')
+        commit.parents.push(field.value)
+      else
+        commit[field.name] = field.value
+    }, function (err) {
+      commit.separateAuthor = (commit.author.str !== commit.committer.str)
+      cb(err, commit)
+    })
+  )
 }
 
 R.getTree = function (ref, cb) {
@@ -155,8 +250,8 @@ R.readTree = function (hash) {
   }
 }
 
-R.readDir = function (branch, path) {
-  var readTree = this.readTree(branch)
+R.readDir = function (rev, path) {
+  var readTree = this.readTree(rev)
 
   if (path.length === 0 || path === '/')
     return readTree
@@ -188,7 +283,7 @@ R.getFile = function (branch, path, cb) {
   var filename = path.pop()
 
   pull(
-    this.readDir(branch, path),
+    this.readDir(rev, path),
     pull.filter(function (file) {
       return file.name === filename
     }),
@@ -196,7 +291,7 @@ R.getFile = function (branch, path, cb) {
     pull.collect(function (err, files) {
       if (err) return cb(err)
       var file = files[0]
-      if (!file === 0)
+      if (!file)
         return cb(new Error('File not found'))
       self.getObject(file.id, function (err, object) {
         if (err) return cb(err)
@@ -210,7 +305,26 @@ R.getFile = function (branch, path, cb) {
   )
 }
 
-R.readLog = function (head, limit) {
+R.getCommit = function (ref, cb) {
+  var self = this
+  this.getRef(ref, function gotRef(err, object) {
+    if (err) return cb(err)
+    switch (object.type) {
+      case 'commit':
+        return cb(null, object, ref)
+      case 'tag':
+        return readCommitOrTagProperty(object, 'object', function (err, hash) {
+          if (err) return cb(err)
+          ref = hash
+          self.getRef(hash, cb)
+        })
+      default:
+        return cb(new Error('Expected commit, got ' + object.type))
+    }
+  })
+}
+
+R.readLog = function (head) {
   var self = this
   return function (end, cb) {
     self.getRef(head, function (err, object) {
